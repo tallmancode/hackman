6e7b046434ddf37ea4bbbe3ca992189dee1f31f0
TallmanCode <steve@tallmancode.co.za>
2024-10-10 11:53:47 +0200

    first commit

---
+++ app.config.ts
@@ -0,0 +1,16 @@
+export default defineAppConfig({
+    ui: {
+        primary: 'purple',
+        card: {
+            body: {
+                padding: 'px-2 py-2 sm:p-2'
+            },
+            header: {
+                padding: 'px-2 py-2 sm:p-2'
+            },
+            footer: {
+                padding: 'px-2 py-2 sm:p-2'
+            }
+        }
+    }
+})
---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div class="w-screen h-screen">
+        <slot></slot>
+    </div>
+</template>
+
+<style scoped></style>
