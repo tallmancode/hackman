30212b0e05a5cfb76e81a648da43feee13a69120
TallmanCode <steve@tallmancode.co.za>
2025-05-28 16:30:23 +0200

    test

--- utils/axios-instance.ts
+++ utils/axios-instance.ts
@@ -45,7 +45,7 @@ export const customAxios = async ({url, method, ...config}) => {
                     return axiosInstance(originalRequest); // Retry the original request with the new access token.
                 } catch (refreshError) {
                     setCookies(undefined, undefined)
-                    window.location.href = '/login';
+                   // window.location.href = '/login';
                     return Promise.reject(refreshError);
                 }
             }
